572d8c66c24052f05859f0a35375af3853c91961
James Austin Hattox <jaustinhattox@gmail.com>
2015-12-11 23:41:05 +0700

    Altered the SMTP password to be correct.

--- server/smtp.js
+++ server/smtp.js
@@ -1,3 +1,3 @@
 Meteor.startup(function(){
-	process.env.MAIL_URL = 'smtp://hello%40jahattox.com:Ada12oogoog@@smtp.gmail.com:465/'
+	process.env.MAIL_URL = 'smtp://hello%40jahattox.com:Ada12oogoog@smtp.gmail.com:465/'
 });
