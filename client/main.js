25e17f8e6e9e8711360fedca6abc3d837ceb6744
James Austin Hattox <jaustinhattox@gmail.com>
2016-01-22 16:14:53 +0700

    Added the projects collection and section with some temporary data.

--- client/main.js
+++ client/main.js
@@ -1,1 +1,3 @@
 Meteor.subscribe('caseStudies');
+
+Meteor.subscribe('projects');
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('caseStudies', function() {
 	return CaseStudies.find({}, {limit: 3, sort: {rating: -1, title: -1}});
 });
+
+Meteor.publish('projects', function() {
+	return Projects.find({}, {limit: 8, sort: {rating: -1, title: -1}});
+});
