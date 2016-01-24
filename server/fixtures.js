41e980239b7dac43955dd3f6a154d8d9727712b6
James Austin Hattox <jaustinhattox@gmail.com>
2016-01-24 15:39:05 +0700

    Changed an incorrect thumbnail src.

--- server/fixtures.js
+++ server/fixtures.js
@@ -34,7 +34,7 @@ if (CaseStudies.find().count() === 0) {
 if (Projects.find().count() === 0) {
 	Projects.insert({
 		title: 'Text Message Form with Twilio',
-		img: '/img/text_message_twilio_form.jpg',
+		img: '/img/text_message_twilio_form_thumbnail.jpg',
 		altText: 'Text Message Form with Twilio',
 		slug: 'text-message-twilio-form',
 		introDescription: "<p>Enter your phone number (U.S. only, please!) and hit send to have an automated text message sent from my phone to yours.</p>",
